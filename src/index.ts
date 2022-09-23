e17d3c4827b2384895672a519be412fa23a4eb2f
Aaron Dalton <aaron@daltons.ca>
2022-09-23 16:29:56 -0600

    Expose genHex and genArcs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./lib/svgLib.js";
 export * as hull from "./lib/hull.js";
 export * as systems from "./lib/systems/index.js";
+export * as hexes from "./lib/genHex.js";
+export * as arcs from "./lib/genArcs.js";
 export type { FullThrustShip } from "./schemas/ship.js";
 
 export const validate = (shipJson: string): boolean => {
