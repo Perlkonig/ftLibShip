63aad28a942d22a3f922f3a009e30eb9e58082fa
Aaron Dalton <aaron@daltons.ca>
2022-09-23 14:40:24 -0600

    OK. Let's try the export * approach

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./lib/svgLib.js";
-export * from "./lib/hull.js"
+export * from "./lib/hull.js";
+export * from "./lib/systems/index.js";
+export type { FullThrustShip } from "./schemas/ship.js";
 
 export const validate = (shipJson: string): boolean => {
     return true;
